4ae46944ed66e1ce0a2708f25a393878054e145f
Raikuman <potatopiehotcakes@gmail.com>
2021-10-06 18:12:45 -0700

    Added Maven Functionality Also added Java Spring

---
+++ src/main/java/com/sharingan/comp380ecommercewebsite/Comp380EcommerceWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.sharingan.comp380ecommercewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp380EcommerceWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp380EcommerceWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharingan/comp380ecommercewebsite/Comp380EcommerceWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharingan.comp380ecommercewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp380EcommerceWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
