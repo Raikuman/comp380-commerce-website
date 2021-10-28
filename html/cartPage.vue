ddb477b2bbb1a07640d0c2c7c0f6fd7303be9f35
Lloyd Kyle Aclan <90808014+showpow381@users.noreply.github.com>
2021-10-27 18:10:24 -0700

    created cart page
    Simple page to nav around

---
+++ html/cartPage.vue
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Cart Page</title>
+		<link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+	<body>
+		<header>
+			<nav>
+				<h1>Cart Page</h1>
+				<ul>
+					<li><a href="checkoutPage.html">Checkout </a></li>
+					<li><a href="cartPage.html">Cart </a></li>
+					<li><a href="productPage.html">Products </a></li>
+					<li><a href="index.html">Homepage</a></li>
+				</ul>
+			</nav>
+		</header>
+
+
+
+
+		<footer>
+			<p>Copyright &copy; 2021, Sharingan Website </p>
+		</footer>
+	</body>
+</html>
