c0f0cdedd73856b70e9c3cb5030437d412fe4664
Lloyd Kyle Aclan <90808014+showpow381@users.noreply.github.com>
2021-10-27 18:09:27 -0700

    Created the main css file

---
+++ css/main.vue
@@ -0,0 +1,44 @@
+body {
+ 	margin: 0;
+ }
+
+header {
+ 	background: #999;
+ 	color: white;
+ 	padding: 0px 0px 0px 5px;
+ }
+
+header h1{
+ 	margin: 0;
+ 	display: inline;
+ }
+
+nav ul {
+	margin: 0;
+	display: inline;
+}
+
+nav ul li {
+ 	background: black;
+ 	color: white;
+ 	display: inline-block;
+ 	list-style-type: none;
+ 	float: right;
+ 	padding: 5px;
+ 	margin: 0px 5px;
+
+ }
+
+nav ul li a {
+ 	color: white;
+
+ }
+
+.product {
+	color: red;
+}
+
+
+footer {
+	text-align: center;
+}
