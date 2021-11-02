9b2a10d5c610e281097a0b2bd964274e2a15ea9b
Raikuman <potatopiehotcakes@gmail.com>
2021-11-01 17:08:56 -0700

    Removed unused imports in ProductRepository.java

--- src/main/java/com/sharingan/comp380ecommercewebsite/product/ProductRepository.java
+++ src/main/java/com/sharingan/comp380ecommercewebsite/product/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.sharingan.comp380ecommercewebsite.product;
 
-import com.sharingan.comp380ecommercewebsite.sqltest.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {}
