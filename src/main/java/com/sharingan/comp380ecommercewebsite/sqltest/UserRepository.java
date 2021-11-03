64549f597b653d74565eedee8fdecf02660afa2c
Raikuman <potatopiehotcakes@gmail.com>
2021-11-03 14:09:56 -0700

    Removed User Classes

--- src/main/java/com/sharingan/comp380ecommercewebsite/sqltest/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sharingan.comp380ecommercewebsite.sqltest;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
