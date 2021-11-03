507bd2b00501d49d142ecc5950a3e8b957d49035
Raikuman <potatopiehotcakes@gmail.com>
2021-11-03 14:17:03 -0700

    Cart Methods need to be completed

--- src/main/java/com/sharingan/comp380ecommercewebsite/product/ProductController.java
+++ src/main/java/com/sharingan/comp380ecommercewebsite/product/ProductController.java
@@ -1,10 +1,8 @@
 package com.sharingan.comp380ecommercewebsite.product;
 
+import com.sharingan.comp380ecommercewebsite.cart.Cart;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,4 +25,8 @@ public class ProductController {
 		return productService.findById(id);
 	}
 
+	@PostMapping
+	public Cart addToCart(@RequestBody Product product) {
+		return productService.saveToCart(product);
+	}
 }
