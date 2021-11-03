6ae70465d00a698d028aa31474d8194fc1114e79
Raikuman <potatopiehotcakes@gmail.com>
2021-11-03 14:22:23 -0700

    Updated Cart ToDo

--- src/main/java/com/sharingan/comp380ecommercewebsite/cart/Cart.java
+++ src/main/java/com/sharingan/comp380ecommercewebsite/cart/Cart.java
@@ -1,4 +1,9 @@
 package com.sharingan.comp380ecommercewebsite.cart;
 
 public class Cart {
+	// TODO: Complete cart class, similar to Product.java
+
+	// TODO: Afterwards, similar to ProductController.java and ProductService.java, need findAll()
+		// TODO: but needs to be on the @RequestMapping("/api/cart")
+		// TODO: the CartController.java will only need the single @GetMapping findAll(), no id needed
 }
