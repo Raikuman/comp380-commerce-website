fa8def76aeae35d42f2efe06aa8ca1a7a77e7d3a
sharkyx101 <90808002+sharkyx101@users.noreply.github.com>
2021-11-15 14:01:51 -0800

    Create CartRepository.java

---
+++ CartRepository.java
@@ -0,0 +1,6 @@
+package com.sharingan.comp380ecommercewebsite.cart;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CartRepository extends CrudRepository<Cart, Integer> {
+}
