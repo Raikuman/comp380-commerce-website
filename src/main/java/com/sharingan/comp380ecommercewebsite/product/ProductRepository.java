7d2de4c998919e3d4de4518275f85733487499d5
Raikuman <potatopiehotcakes@gmail.com>
2021-11-22 17:10:48 -0800

    Added documentation

--- src/main/java/com/sharingan/comp380ecommercewebsite/product/ProductRepository.java
+++ src/main/java/com/sharingan/comp380ecommercewebsite/product/ProductRepository.java
@@ -2,4 +2,12 @@ package com.sharingan.comp380ecommercewebsite.product;
 
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * This class provides an object that reflects the database. In this case, it is a repository object of
+ * products from our database
+ *
+ * @author Jim Inong
+ * @since 2021-22-11
+ * @version 1.0
+ */
 public interface ProductRepository extends CrudRepository<Product, Integer> {}
--- src/main/java/com/sharingan/comp380ecommercewebsite/product/ProductService.java
+++ src/main/java/com/sharingan/comp380ecommercewebsite/product/ProductService.java
@@ -7,24 +7,66 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * This class provides the business logic for the product objects and provides these methods mainly to the
+ * endpoint class, ProductController.java
+ *
+ * @author Jim Inong
+ * @since 2021-22-11
+ * @version 1.0
+ */
 @Service
 public class ProductService {
 
+	/**
+	 * Provides an instance of the product repository from the database
+	 */
 	@Autowired
 	private ProductRepository productRepository;
 
+	/**
+	 * Provides an instance of the cart repository from the database
+	 */
 	@Autowired
 	private CartRepository cartRepository;
 
+	/**
+	 * This method returns a list of products from the product repository
+	 * @return Returns a list of products
+	 */
 	public List<Product> findAll() {
 		return (List<Product>) productRepository.findAll();
 	}
 
+	/**
+	 * This method gets the product from the database using the id given
+	 * @param id Provides the id to find a product
+	 * @return Returns the product found in the database
+	 */
 	public Product findById(int id) {
 		return productRepository.findById(id).orElseThrow(RuntimeException::new);
 	}
 
+	/**
+	 * This method saves a given cart object to the database. Before saving, it checks if there is already
+	 * an existing cart object with the same productId. If there is an existing cart object, it updates it
+	 * so that the quantity of the existing cart and the current cart is added, then saved to the database
+	 * @param cartItem Provides the cart object to save to the database
+	 * @return Returns the saved cart item
+	 */
 	public Cart saveToCart(Cart cartItem) {
+
+		// Update quantity of currently existing product if applicable
+		for (Cart cart : cartRepository.findAll()) {
+			if (cart.getProductId() == cartItem.getProductId()) {
+				Cart temp = new Cart(cart.getProductId(), (cart.getQuantity()) + cartItem.getQuantity());
+
+				cartRepository.delete(cart);
+				cartRepository.save(temp);
+				return temp;
+			}
+		}
+
 		cartRepository.save(cartItem);
 		return cartItem;
 	}
