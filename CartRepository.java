c7eed1c60a0066f855bcd6067c41c20d93fd2e81
Raikuman <potatopiehotcakes@gmail.com>
2021-11-23 04:10:45 -0800

    Remove unused files

--- CartRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sharingan.comp380ecommercewebsite.cart;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CartRepository extends CrudRepository<Cart, Integer> {
-}
