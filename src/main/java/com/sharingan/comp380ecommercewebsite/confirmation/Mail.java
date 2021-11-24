140d783fd892afcd97efef955733eea6877333e9
Raikuman <potatopiehotcakes@gmail.com>
2021-11-23 19:21:43 -0800

    Updated Mail.java Removed sensitive information from the class

---
+++ src/main/java/com/sharingan/comp380ecommercewebsite/confirmation/Mail.java
@@ -0,0 +1,103 @@
+package com.sharingan.comp380ecommercewebsite.confirmation;
+
+import com.sharingan.comp380ecommercewebsite.config.ConfigHandler;
+
+import java.util.*;
+import javax.mail.*;			//Java.mail is an external java file used to write an email using mimemessage.
+import javax.mail.internet.*;
+import javax.activation.*;     //Java.activation is an external java file used to transport and send the email.
+
+/**
+ * This class is used to send a thank you email to a respective client after they had made a purchase.
+ * The class uses a SMTP server provided by google using a gmail account.
+ * The email used was created for this project specifically and contians its password (Careful X-X)
+ *
+ * @author Gerardo Huerta
+ * @since 2021-20-11
+ */
+public class Mail{
+
+	/**
+	 * This method is used to setup an email, attach and create all of its components and send email to respective user
+	 */
+	public static void sendEmail() {
+
+		//Email that will be used for authentication
+		final String username = ConfigHandler.loadConfigSetting("email");
+
+		//password for the email being used to authenticate
+		final String password = ConfigHandler.loadConfigSetting("password");
+
+		//final String host = "smtp.gmail.com";
+
+		// Get system properties
+		Properties props = new Properties();
+
+		// enable authentication the email is using googles smtp server which is free
+		props.put("mail.smtp.auth", "true");
+
+		// enable STARTTLS
+		props.put("mail.smtp.starttls.enable", "true");
+
+		// Setup mail server
+		props.put("mail.smtp.host", "smtp.gmail.com");
+
+		// TLS Port
+		props.put("mail.smtp.port", ConfigHandler.loadConfigSetting("tlsport"));
+
+		// creating Session instance referenced to
+		// Authenticator object to pass in
+		// Session.getInstance argument
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+
+			// override the getPasswordAuthentication method
+			protected PasswordAuthentication getPasswordAuthentication() {
+
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		try {
+
+			// compose the message
+			// javax.mail.internet.MimeMessage class is
+			// mostly used for abstraction.
+			Message message = new MimeMessage(session);
+			String receipient = ConfigHandler.loadConfigSetting("sendmail");;//example email, needs to be
+			// implemented to call current user's email address //still not complete
+
+			// header field of the header.
+			assert username != null;
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receipient));
+
+			message.setSubject("Thank You For Your Purchase");
+			// creating first MimeBodyPart object
+			BodyPart messageBodyPart1 = new MimeBodyPart();
+			messageBodyPart1
+					.setText("We are thankful for your purchase! :D" + System.lineSeparator() + "Thank you " +
+						"for shopping with us. Hope to see you soon!" + System.lineSeparator() + "Here is your receipt. Hava a nice day! UwU");
+
+			// creating second MimeBodyPart object
+			BodyPart messageBodyPart2 = new MimeBodyPart();
+			String filename = "receipt.txt"; //This part is supposed to take a document and send it as an attachment in the email.
+			DataSource source = new FileDataSource(filename);
+			messageBodyPart2.setDataHandler(new DataHandler(source));
+			messageBodyPart2.setFileName(filename);
+
+			// creating MultiPart object
+			Multipart multipartObject = new MimeMultipart();
+			multipartObject.addBodyPart(messageBodyPart1);
+			multipartObject.addBodyPart(messageBodyPart2);
+
+			// set body of the email.
+			message.setContent(multipartObject);
+			Transport.send(message); // send Message
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
