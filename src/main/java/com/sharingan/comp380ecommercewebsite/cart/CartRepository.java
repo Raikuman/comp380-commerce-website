ee76dcc3923dc5a5f155bcbe6f5ee15c0a35e0b1
sharkyx101 <90808002+sharkyx101@users.noreply.github.com>
2021-11-23 19:23:19 -0800

    Update CartRepository.java

--- src/main/java/com/sharingan/comp380ecommercewebsite/cart/CartRepository.java
+++ src/main/java/com/sharingan/comp380ecommercewebsite/cart/CartRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 
 /**
  *
- *
+ * @extends CartRepository to CrudRespository
  * @author Michael Sanati
  * @since 2021-15-11
  */
+
 public interface CartRepository extends CrudRepository<Cart, Integer> { }
