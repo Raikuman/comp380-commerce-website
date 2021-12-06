97062caa2220071d1087c1854701deda26a15632
Raikuman <potatopiehotcakes@gmail.com>
2021-12-06 13:56:55 -0800

    Provides information from the checkout

---
+++ src/main/java/com/sharingan/comp380ecommercewebsite/other/CheckoutObj.java
@@ -0,0 +1,16 @@
+package com.sharingan.comp380ecommercewebsite.other;
+
+public class CheckoutObj {
+
+	private String email;
+
+	private String total;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTotal() {
+		return total;
+	}
+}
